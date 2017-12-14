8389fe2ffd629d51288efd8929fcc034cd9c4372
Gabriel Pérez S <gabriel@guero.net>
2017-12-14 11:32:11 +0100

    Remove calculation functions and rename variables

--- monad.go
+++ monad.go
@@ -1,26 +1,24 @@
 package monad
 
 import (
+	"log"
 	"sync"
 	"time"
 )
 
 var (
-	defaultInterval  = time.Duration(500 * time.Millisecond)
-	defaultCooldDown = time.Duration(10 * time.Second)
+	defaultInterval        = time.Duration(500 * time.Millisecond)
+	defaultCooldDownPeriod = time.Duration(10 * time.Second)
 )
 
 type Config struct {
-	Min                int64
-	Max                int64
-	Desired            int64
-	ThresholdScaleUp   int64
-	ThresholdScaleDown int64
-	Interval           time.Duration
-	CoolDownPeriod     time.Duration
-	LenFn              func() int64
-	DesireFn           func(n int64)
-	StatusFn           func() bool
+	Min            uint64
+	Max            uint64
+	Desired        uint64
+	Interval       time.Duration
+	CoolDownPeriod time.Duration
+	WarmFn         func() bool
+	DesireFn       func(n uint64)
 }
 
 type Monad struct {
@@ -28,13 +26,15 @@ type Monad struct {
 	cfg         *Config
 	t           *time.Ticker
 	done        chan struct{}
-	desired     int64
+	finish      chan struct{}
+	desired     uint64
 	lastActivty time.Time
 }
 
 func New(cfg *Config) *Monad {
 	m := &Monad{
-		done: make(chan struct{}, 1),
+		done:   make(chan struct{}, 1),
+		finish: make(chan struct{}, 1),
 	}
 	m.Reload(cfg)
 
@@ -55,46 +55,58 @@ func (m *Monad) Reload(newCfg *Config) {
 	}
 
 	if m.cfg.CoolDownPeriod.Nanoseconds() == 0 {
-		m.cfg.CoolDownPeriod = defaultCooldDown
+		m.cfg.CoolDownPeriod = defaultCooldDownPeriod
 	}
 
 	if m.t == nil {
 		m.t = time.NewTicker(m.cfg.Interval)
 	} else {
-		m.t.Stop()
+		m.done <- struct{}{}
+		<-m.finish
 		m.t = time.NewTicker(m.cfg.Interval)
+		go m.monitor()
 	}
 }
 
 func (m *Monad) Exit() {
 	m.done <- struct{}{}
+	<-m.finish
 }
 
 func (m *Monad) monitor() {
+	defer func() {
+		m.finish <- struct{}{}
+	}()
+
 	for {
 		select {
 		case <-m.done:
 			m.t.Stop()
-			m.t = nil
 			return
 		case <-m.t.C:
 
-			l := m.cfg.LenFn()
+			l := m.cfg.WarmFn()
 
 			switch {
-			case l > m.cfg.ThresholdScaleUp && m.desired <= m.cfg.Max:
+			case l == true && m.desired < m.cfg.Max:
 				m.desired++
 				m.cfg.DesireFn(m.desired)
 				m.lastActivty = time.Now()
 				break
-			case m.desired > 0 && l < m.cfg.ThresholdScaleDown && m.desired > m.cfg.Min:
+			case l == false && m.desired > m.cfg.Min:
 				if m.lastActivty.Add(m.cfg.CoolDownPeriod).Before(time.Now()) {
 					m.desired--
 					m.cfg.DesireFn(m.desired)
 					m.lastActivty = time.Now()
 				}
 				break
+			case m.desired > m.cfg.Max:
+				m.desired--
+				m.cfg.DesireFn(m.desired)
+				m.lastActivty = time.Now()
 			}
+
+			log.Printf("Queue: %v, Desired %d, lastActivity: %s", l, m.desired, m.lastActivty)
 
 		}
 	}
